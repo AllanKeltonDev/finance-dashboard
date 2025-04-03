f81bacb4bb47cb21a0c981d2ff3a89723d9410a2
AllanKeltonDev <105874973+AllanKeltonDev@users.noreply.github.com>
2025-04-03 18:39:54 -0300

    Inicialização do projeto com estrutura básica e componente App

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+<h1>Hello World!</h1>
+
+      </div>
+      <h1>Dashboard de Controle Financeiro</h1>
+      <p>
+        Alterando Entrada!!
+      </p>
+    </>
+  )
+}
+
+export default App
